feba76399419f5f156dfa053924ed24e6ffc4f8d
Kev Grav <kgravell.prsvr@gmail.com>
2025-06-08 15:29:42 -0400

    initial commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import AboutImage from "../assets/aboutme-image.jpg";
 
 const About = () => {
   return (
-    <div id="about" className="bg-blue-900 text-white py-10" >
+    <div id="about" className="bg-blue-900 text-white py-1 scroll-mt-24" >
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12" >About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -37,7 +37,7 @@ const projects = [
 
 const Projects = () => {
   return (
-    <section id="projects" className="bg-blue-900 text-white py-20" >
+    <section id="projects" className="bg-blue-900 text-white py-20 scroll-mt-24" >
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
